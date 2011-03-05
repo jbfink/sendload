184857e070c0f5b18d1e037843c6a2d4196cd737
jbfink <john.fink@gmail.com>
2011-03-04 22:15:38 -0500

    Man. puts, putc, print, none of 'em work. What am I doing wrong?

--- sendload.rb
+++ sendload.rb
@@ -19,7 +19,7 @@ loop do
   loadavg = raw.scan(/^.{4}/)[0].to_f
 #  puts f
   puts loadavg
-  sp.putc(loadavg)
+  sp.print(loadavg)
   puts 'Wrote: ' + loadavg.to_s
 #  i = (i == 15) ? 0 : (i + 1)
   f.close
